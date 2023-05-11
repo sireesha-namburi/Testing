3997b6fee61e2aeb23bae0d774deb480c1ed8ed2
Sireesha Namburu <sn23023@corp.ojas-it.com>
2023-05-11 12:13:53 +0530

    first commit

---
+++ EvenOdd.java
@@ -0,0 +1,25 @@
+package topcrowd;
+import java.util.*;
+public class EvenOdd {
+	// in master
+	private static int oddRounder(int num)
+	{
+		int temp;
+		if(num<0)return -1;
+		else 
+			if(num == 0) return -2;
+			else if((num%2) == 0)return num;
+			      else {temp= ((num/10) +1)*10;return temp;}
+		
+	}
+	public static void main(String[] args)
+	{
+		int num,temp;
+		System.out.println("Enter any number :");
+		Scanner sc = new Scanner(System.in);
+		num= sc.nextInt();		
+		temp = oddRounder(num);
+		System.out.println(temp);
+		
+	}
+}
